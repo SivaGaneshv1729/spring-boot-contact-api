f34e1270f005c4d81d016eeebb1a1348ddbc9009
Siva Ganesh <sivaganeshv1729@gmail.com>
2026-06-02 19:21:44 +0530

    "feat-preserve-swagger-exceptions-snapshot-with-clean-history"

---
+++ src/main/java/com/virax/restapi/contact_api/controller/ContactController.java
@@ -0,0 +1,69 @@
+package com.virax.restapi.contact_api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.virax.restapi.contact_api.dtos.ContactDto;
+import com.virax.restapi.contact_api.service.ContactOperations;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class ContactController {
+	
+	@Autowired
+	ContactOperations co;
+	
+	@PostMapping("add/contact")
+	public ResponseEntity<Boolean> addContact(@RequestBody ContactDto contactDto) {
+		return new ResponseEntity<>(co.addContact(contactDto),HttpStatus.CREATED);
+	}
+	
+	@PostMapping("add/multi/contact")
+	public ResponseEntity<List<ContactDto>> addMultipleContact(@RequestBody List<ContactDto> ip) {
+		return new ResponseEntity<>(co.addMultipleContacts(ip), HttpStatus.MULTI_STATUS);
+	}
+	
+	@GetMapping("get/contact/{mobileNumber}")
+	public ResponseEntity<ContactDto> getContact(@PathVariable String mobileNumber) {
+		return new ResponseEntity<>(co.getContactByMobileNumber(mobileNumber), HttpStatus.OK);
+	}
+	
+	@GetMapping("get/all/contacts")
+	public ResponseEntity<List<ContactDto>> gatAllContacts(){
+		return new ResponseEntity<>(co.getAllContacts(),HttpStatus.OK);
+	}
+	
+	@PutMapping("/update/name/{mobileNumber}")
+	public ResponseEntity<String> updateName(@PathVariable String mobileNumber, @RequestBody String name) {
+		co.updateContactName(mobileNumber, name);
+		return new ResponseEntity<>("Contact Name Updated..!", HttpStatus.OK);
+	}
+	
+	@PutMapping("/updateByNumber/{mobileNumber}")
+	public ResponseEntity<String> updateContact(@PathVariable String mobileNumber, @RequestBody ContactDto c) {
+		co.updateContact(mobileNumber, c);
+		return new ResponseEntity<>("Contact Updated..!", HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteByNumber/{mobileNumber}")
+	public ResponseEntity<String> deleteContact(@PathVariable String mobileNumber) {
+		co.deleteContactByMobileNumber(mobileNumber);
+		return new ResponseEntity<>("Contact Deleted.",HttpStatus.OK);
+	}
+	
+	@GetMapping("/search/{key}")
+	public ResponseEntity<List<ContactDto>> search(@PathVariable String key) {
+		return new ResponseEntity<>(co.searchContacts(key), HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/virax/restapi/contact_api/service/ContactOperations.java
@@ -0,0 +1,98 @@
+package com.virax.restapi.contact_api.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.virax.restapi.contact_api.dtos.ContactDto;
+import com.virax.restapi.contact_api.mappers.ContactMapper;
+import com.virax.restapi.contact_api.model.Contact;
+import com.virax.restapi.contact_api.repository.ContactRepository;
+
+@Service
+
+public class ContactOperations {
+	
+	@Autowired
+	ContactRepository contactRepository;
+	@Autowired
+	ContactMapper contactMapper;
+	
+	Logger logger = LogManager.getLogger(ContactOperations.class);
+	
+	public boolean addContact(ContactDto c) {
+		
+		if (contactRepository.findById(c.getMobileNumber()).isPresent()) {
+			logger.info("Contact already exist with number" + c.getMobileNumber());
+			throw new ResponseStatusException(HttpStatus.CONFLICT, "MObile Number Already Exists");
+		}
+		else {
+			contactRepository.save(contactMapper.toContact(c));
+			return true;
+		}
+	}
+	
+	
+	public List<ContactDto> addMultipleContacts(List<ContactDto> contactDtos) {
+		List<Contact> savedContacts = contactRepository.saveAll(contactMapper.toContacts(contactDtos));
+		return contactMapper.toContactDtos(savedContacts);
+	}
+	
+	public ContactDto getContactByMobileNumber(String mobileNumber) {
+		Contact contact = contactRepository.findById(mobileNumber)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "MObile Number Not Found"));
+		return contactMapper.toContactDto(contact);
+	}
+	
+	public List<ContactDto> getAllContacts() {
+		return contactMapper.toContactDtos(contactRepository.findAll());
+	}
+	
+	public List<ContactDto> getContactsByType(String type){
+		return contactMapper.toContactDtos(contactRepository.getContactsByType(type));
+	}
+	
+	public void updateContactName(String mobileNumber, String newName) {
+		if(!contactRepository.existsById(mobileNumber)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Mobile Number Not Found");
+		}
+		contactRepository.updateContactName(mobileNumber, newName);
+		logger.info("name updated for contact with number " + mobileNumber);
+	}
+	
+	public void updateContact(String mobileNumber, ContactDto contactDto) {
+		if(!contactRepository.existsById(mobileNumber)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Mobile Number Not Found");
+		}
+		contactRepository.updateContact(mobileNumber,contactMapper.toContact(contactDto));
+		logger.info("Contact Updated with " + mobileNumber);
+	}
+	public void deleteContactByMobileNumber(String mobileNumber) {
+		if(!contactRepository.existsById(mobileNumber)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Contact Exists to Delete");
+		}
+		contactRepository.deleteById(mobileNumber);
+		logger.info("contact deleted with number " + mobileNumber);
+	}
+	public List<ContactDto> searchContacts(String key){
+		List<Contact> arr = contactMapper.toContacts(getAllContacts());
+		List<Contact> res = new ArrayList<>();
+		
+		for (Contact c: arr) {
+			String dataString = c.getMobileNumber() + " " + c.getEmail() + " " + c.getName() + " " + c.getType();
+			
+			if(dataString.contains(key)) {
+				res.add(c);
+			}
+		}
+		return contactMapper.toContactDtos(res);
+	}
+}
